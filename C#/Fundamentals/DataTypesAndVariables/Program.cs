a8c8c3f258f85d28cabe675b2fd6456186e33bb9
Jiří Janeček <jiri.janecek@avg.com>
2016-05-25 13:59:37 +0200

    +added new projects from C# fundamentals series

---
+++ C#/Fundamentals/DataTypesAndVariables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesAndVariables
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 7;
+			int y = x + 3;
+			Console.WriteLine(y);
+
+			readingFromConsole();
+
+			Console.ReadLine();
+			return;
+		}
+
+		static void readingFromConsole(			
+			)
+		{
+			Console.Write("Given me anything: ");
+			String anything = Console.ReadLine();
+
+			String answer = "You give me: " + anything;
+			Console.WriteLine(answer);
+
+			return;
+		}
+	}
+}
