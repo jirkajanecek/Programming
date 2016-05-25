be3ef90fc1953cdd42d3c3de2431fcfd8099ef00
Jiří Janeček <jiri.janecek@avg.com>
2016-05-25 18:30:03 +0200

    +Array lesson

---
+++ C#/Fundamentals/Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = new int[5];
+
+			for(int i = 0; i < numbers.Length; ++i)
+			{
+				numbers[i] += i + 7;
+			}
+
+			int[] numbers2 = new int[numbers[0]];
+
+
+			///////////////////////////////////////////////////////////////////
+
+			int[] numbers3 = new int[] { 0, 12, 3, 548, 98, 68 };
+
+			string[] strings = new string[] { "ahoj", "nazdar", "cau" };			
+
+			foreach (string item in strings)
+			{
+				Console.WriteLine(item);
+			}
+
+			///////////////////////////////////////////////////////////////////
+
+			string longSentence = "Very long sentence...";
+			char[] chars = longSentence.ToCharArray();
+			Array.Reverse(chars);
+
+			///////////////////////////////////////////////////////////////////
+		}
+	}
+}
