1d955abd6a71f4786c1a3e260f5f9d9a4f9d3732
Jiří Janeček <Ji Janeek>
2016-07-31 21:39:18 +0200

    Exceptions fundamental

---
+++ C#/Fundamentals/Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				string content = File.ReadAllText(@"D:\temp\wtf.txt");
+				Console.WriteLine("Reading...");				
+			}
+			catch (IOException e)
+			{
+				Console.WriteLine("Catching...");
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Cleaning...");
+			}
+
+			Console.WriteLine("Ending...");
+			Console.ReadLine();
+		}
+	}
+}
