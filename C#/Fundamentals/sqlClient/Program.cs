aa361032dab00eec64b8270665e8d45f2ef1a478
Jiří Janeček <jiri.janecek@avg.com>
2016-10-17 17:21:35 +0200

    Some excercises from threads and SQL client

---
+++ C#/Fundamentals/sqlClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace sqlClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SqlConnection sqlConnection = null;
+			DBTester test = new DBTester();
+			test.Connect("virtestdb.cz.avg.com", "test_janecekj", "janecekj", "weclopusis", ref sqlConnection);
+			if(sqlConnection != null)
+			{
+				test.ProcessInsert(ref sqlConnection);
+				test.ProcessSelect(ref sqlConnection);
+				sqlConnection.Close();
+			}					
+		}
+	}
+}
