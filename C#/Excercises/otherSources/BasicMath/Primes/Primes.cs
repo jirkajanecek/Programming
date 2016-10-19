1ec9a0f7dec64c7309ca38ea5a3bce1d51b1459b
Jiří Janeček <Ji Janeek>
2016-10-19 21:52:03 +0200

    more from excercises and some fundamentals on structs

---
+++ C#/Excercises/otherSources/BasicMath/Primes/Primes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+	public class Primes
+	{
+		public static void processTests()
+		{
+			FindPrimesOnInterval();
+			Factorization();
+		}
+
+		public static void FindPrimesOnInterval()
+		{
+			int from = 2;
+			int to = 37;
+			List<int> primes = new List<int>();
+			
+			for(int current = from; current <= to; ++current)
+			{
+				if(current <= 1)
+				{
+					continue;
+				}
+
+				bool hit = false;
+
+				for(int tested = 2; tested < current; ++tested)
+				{
+					if(current % tested == 0)
+					{
+						hit = true;
+						break;
+					}
+				}
+
+				if (hit == false)
+				{
+					primes.Add(current);
+				}
+			}
+
+			foreach(int prime in primes)
+			{
+				Console.Write("{0} ", prime);				
+			}
+			Console.WriteLine();
+		}
+
+		public static void Factorization()
+		{
+			int number = 420;
+			int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
+			int primeIndex = 0;
+
+			while(true)
+			{
+				int remain = number % primes[primeIndex];
+				if(remain != 0)
+				{
+					primeIndex++;
+					continue;
+				}
+				Console.Write("{0} ",primes[primeIndex]);
+				number = number / primes[primeIndex];
+				if(number == 1)
+				{
+					break;
+				}				
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ C#/Fundamentals/Structs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+	class Program
+	{
+		struct TestStruct
+		{
+			public int one;
+			public int two;
+
+			public TestStruct(
+				int givenOne,
+				int givenTwo)
+			{
+				one = givenOne;
+				two = givenTwo;
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			TestStruct test01 = new TestStruct(1,2);
+			TestStruct test02 = new TestStruct();
+			test02 = test01;
+
+			TestStruct test03;
+			test03.one = 1;
+			test03.two = 2;
+			test02 = test03;
+
+			Console.ReadKey();
+		}
+	}
+}
