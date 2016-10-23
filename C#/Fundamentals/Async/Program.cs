74c5ea164fda383c1a6ef22d7bb2782593b4acf1
Jiří Janeček <Ji Janeek>
2016-10-23 22:30:58 +0200

    some basics about asynchronious programming

---
+++ C#/Fundamentals/Async/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Basic.processTestAsync().Wait();
+			Console.ReadKey();
+		}
+	}
+}
