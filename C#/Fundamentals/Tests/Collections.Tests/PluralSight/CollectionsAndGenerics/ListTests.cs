00454bed005aa476d2c434c59388d3783e8aece7
Jiri Janecek <jiri.janecek@solarwinds.com>
2017-02-01 15:39:24 +0100

    + the first part (List) of collection snippet from plural sight course with tests

---
+++ C#/Fundamentals/Tests/Collections.Tests/PluralSight/CollectionsAndGenerics/ListTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Collections.PluralSight.CollectionsAndGenerics;
+
+namespace Collections.Tests.PluralSight.CollectionsAndGenerics
+{
+	[TestFixture]
+	class ListTests
+	{
+		string[] m_originalArray = { "item3", "item2", "item1", "item0" };
+
+		[Test]
+		public void List_Add()
+		{
+			var newElement = "item4";
+			string[] expected = { "item3", "item2", "item1", "item0", newElement };
+
+			List<string> list = new List<string>(m_originalArray);
+			var obtained = list.AddElement(newElement);
+
+			CollectionAssert.AreEqual(expected, obtained, "adding new element into list failed");
+		}
+	}
+}
