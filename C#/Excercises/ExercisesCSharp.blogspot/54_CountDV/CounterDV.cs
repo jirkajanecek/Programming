07e69a2daf3fc118dd212c4f4666ff6d7a2d0bf9
Jiri Janecek <jirka.janecek@gmail.com>
2017-03-05 18:57:49 +0100

    Excercise: digits and vovels counter with unit test

---
+++ C#/Excercises/ExercisesCSharp.blogspot/54_CountDV/CounterDV.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54_CountDV
+{
+	public class CounterDV
+	{
+		static public void CountDV(string text, ref int digits, ref int vovels)
+		{
+			var arrayedText = text.ToLower().ToCharArray();
+
+			digits = arrayedText.Where(v => char.IsDigit(v)).Count();
+			vovels = arrayedText.Where(v => IsVovel(v)).Count();
+
+			return;
+		}
+
+		static private bool IsVovel(char b)
+		{
+			var vovels = new char[]{ 'a', 'e', 'i', 'o', 'u', 'y'};
+			return vovels.Contains(char.ToLower(b));
+		}
+	}
+}
---
+++ C#/Excercises/ExercisesCSharp.blogspot/Tests/54_CountDV.Tests/CounterDVTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using _54_CountDV;
+
+namespace _54_CountDV.Tests
+{
+	[TestFixture]
+	class CounterDVTests
+	{
+		[TestCase("AHoj1234", 4, 2)]
+		[TestCase("Some1Other2Text3", 3, 5)]
+		public void _54_CountDV_Suceeded(string text, int expectedDigits, int expectedVovels)
+		{
+			int actualDigits = 0;
+			int actualVovels = 0;
+
+			CounterDV.CountDV(text, ref actualDigits, ref actualVovels);
+
+			Assert.AreEqual(expectedDigits, actualDigits, "digits should be the same");
+			Assert.AreEqual(expectedVovels, actualVovels, "vovels should be the same");
+		}
+	}
+}
